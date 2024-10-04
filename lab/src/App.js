95946a01b096e0721a4f34b40ac11ce3bd464555
Александр Шаройко <himanru3@gmail.com>
2024-10-04 15:45:10 +0300

    feat: add GetStarted component

--- lab/src/App.js
+++ lab/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from'./Components/HeaderComp/HeaderComp.jsx'
 import Footer from './Components/FooterComp/FooterComp.jsx'
 import ShopByRoom from './Components/ShopByRoomComp/ShopByRoomComp.jsx';
+import GetStarted from './Components/GetStartedComp/GetStartedComp.jsx';
 
 function App() {
   return (
     <>
       <Header/>
       <main>
+        <GetStarted/>
         <ShopByRoom/>
       </main>
       <Footer/>
--- lab/src/Components/ShopByRoomComp/ShopByRoomComp.jsx
+++ lab/src/Components/ShopByRoomComp/ShopByRoomComp.jsx
@@ -1,20 +1,38 @@
 import { Component } from "react";
 import '../ShopByRoomComp/ShopByRoom.css'
-import Main_Container1_img from '../../img/Main_Container1_img.png'
+import Main_container2_rect_1 from '../../img/Main_container2_rect_1.png'
+import Main_container2_rect_2 from '../../img/Main_container2_rect_2.png'
+import Main_container2_rect_3 from '../../img/Main_container2_rect_3.png'
+import Main_container2_rect_4 from '../../img/Main_container2_rect_4.png'
+
 class ShopByRoom extends Component{
     render(){
         return(
             
-        <div id="Main_container1" className="Main_container1">
-            <div className="Main_container1_left_div">               
-                    <p id="Curated_Conv" className="Main_container1_top_text">Curated and Convenient</p>
-                    <p id= "Main_1Text" className="Main_container1_bottom_text">We've built our offerings on the principle that everyone deserves high-quality design without the high cost. We offer elevated rental inventory, to transform your space. Reuse and recycle - we make the decision as light as a “feather”</p>
-                    <button id="Main_1_button" className="Main_container1_button">Get Started</button>
+            <div id="Main_container2" className="Main_container2">
+                <div class="Main_container2_text">
+                <p id="Shop_by_Room" >Shop by Room</p> 
+                </div>
+                <div id="Main_container2_rectangles" className="Main_container2_rectangles">
+                    <div id="Main_container2_rectangle1" onclick="window.location.href='livingroom.html';" class="Main_container2_rectangle1">
+                        <img src={Main_container2_rect_1} alt=""></img>
+                        <p id="Living_room">Living Room</p>
+                    </div>
+                    <div id="Main_container2_rectangle2" onclick="window.location.href='Bedroom.html';" class="Main_container2_rectangle2">
+                        <img src={Main_container2_rect_2} alt=""></img>
+                        <p id="Bedroom">Bedroom</p>
+                    </div>
+                    <div id="Main_container2_rectangle3" onclick="window.location.href='Office.html';" class="Main_container2_rectangle3">
+                        <img src={Main_container2_rect_3} alt=""></img>
+                        <p id="Home_Office">Home Office</p>
+                    </div>
+                    <div id="Main_container2_rectangle4" onclick="window.location.href='Diningroom.html';" class="Main_container2_rectangle4">
+                        <img src={Main_container2_rect_4} alt=""></img>
+                        <p id="Dining_Room">Dining Room</p>
+                    </div>                
+                </div>
             </div>
-            <div className="Main_container1_img"> 
-                <img src={Main_Container1_img} alt=""></img>
-            </div>
-        </div>            
+             
         )
     }
 }
